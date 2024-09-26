7eefcd65d823b2b9a3046c53162dae480b783767
egorka <egorka.smakov04@gmail.com>
2024-09-26 11:43:18 +0300

    задание 4_3

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 public class Hello{
     public static void main(String[] args) {
-        System.out.println("Hello worldB");
-	System.out.println("B");
+	System.out.println("Hello world2");
     }
 }
