9a26ef1c663a854cc8c535de29bf00a49e502a3d
egorka <egorka.smakov04@gmail.com>
2024-09-26 11:26:23 +0300

    задание 2 2-ая копия

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
     public static void main(String[] args) {
-        System.out.printf("Hello world");
+        System.out.printf("Hello worldB");
+	System.out.println("B");
     }
 }
